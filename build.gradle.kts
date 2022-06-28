e5c31bbf048933ef4d84b39a14a528e626531b64
karenmillermoj <karen.miller@digital.justice.gov.uk>
2022-06-28 16:48:32 +0100

    HAAR-584 Add Test endpoints for testing with hmpps-authorization-server

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,9 @@ configurations {
 }
 
 dependencies {
+  implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+  implementation("org.springframework.boot:spring-boot-starter-security")
+
   implementation("org.springframework.boot:spring-boot-starter-webflux")
 }
 
