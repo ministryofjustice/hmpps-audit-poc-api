02a879ea06f7f679b3d9e928d02db019d3569f94
simonmitchell <simon.mitchell@digital.justice.gov.uk>
2024-01-24 13:59:44 +0000

    HAAR-2281: ⬆️ update to spring-boot-plugin-5.15.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.14.1"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.1"
   kotlin("plugin.spring") version "1.9.22"
 }
 
@@ -19,7 +19,7 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
 
-  implementation(platform("com.amazonaws:aws-java-sdk-bom:1.12.638"))
+  implementation(platform("com.amazonaws:aws-java-sdk-bom:1.12.643"))
 }
 
 kotlin {
