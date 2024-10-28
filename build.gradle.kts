ecffd9f2f953cd917e26d358c349240cae7ed049
ma226860 <142583965+ma226860@users.noreply.github.com>
2024-10-28 15:51:24 +0000

    HAAR-3187: ⬆️ Upgrading to Spring Boot 3.3.5 to resolve CVE (#125)
    * HAAR-3187: ⬆️ Upgrading to Spring Boot 3.3.5 to resolve CVE
    
    * build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.6"
-  kotlin("plugin.spring") version "2.0.20"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.8"
+  kotlin("plugin.spring") version "2.0.21"
 }
 
 configurations {
@@ -14,12 +14,12 @@ repositories {
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-security")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:4.4.4")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.0.2")
 
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
 
-  implementation(platform("com.amazonaws:aws-java-sdk-bom:1.12.772"))
+  implementation(platform("com.amazonaws:aws-java-sdk-bom:1.12.777"))
 }
 
 kotlin {
