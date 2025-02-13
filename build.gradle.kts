0e38344f833b6d4021b762ec0f2d8832d9470a1c
Simon Mitchell <51327941+simon-mitchell@users.noreply.github.com>
2025-02-13 15:04:00 +0000

    HAAR-3635: ⬆️ update to spring-boot-plugin-7.1.2 (#133)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "7.1.1"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "7.1.2"
   kotlin("plugin.spring") version "2.1.10"
 }
 
@@ -10,12 +10,12 @@ configurations {
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-security")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.3.0")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.3.1")
 
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.4")
 
-  implementation(platform("com.amazonaws:aws-java-sdk-bom:1.12.780"))
+  implementation(platform("com.amazonaws:aws-java-sdk-bom:1.12.781"))
 }
 
 kotlin {
