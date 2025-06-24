f12122cd373692f4520208a62de9c85a783cf9c4
Simon Mitchell <51327941+simon-mitchell@users.noreply.github.com>
2025-06-24 14:43:49 +0100

    HAAR-4059: ⬆️ update to spring-boot-plugin-8.3.0 (#140)
    * HAAR-4059: ⬆️ update to spring-boot-plugin-8.3.0
    
    * HAAR-4059: revert kotlin plugin upgrade
    
    * HAAR-4059: upgrade helm chart versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.2.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.3.0"
   kotlin("plugin.spring") version "2.1.21"
 }
 
@@ -13,9 +13,9 @@ dependencies {
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.4.5")
 
   implementation("org.springframework.boot:spring-boot-starter-webflux")
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.8")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.9")
 
-  implementation(platform("com.amazonaws:aws-java-sdk-bom:1.12.783"))
+  implementation(platform("com.amazonaws:aws-java-sdk-bom:1.12.787"))
 }
 
 kotlin {
