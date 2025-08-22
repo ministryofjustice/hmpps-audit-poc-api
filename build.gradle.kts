943d32563758500fc695f9cdb06a4b8a5ae1ad1e
simon.mitchell <simon.mitchell@digital.justice.gov.uk>
2025-08-22 13:52:03 +0100

    HAAR-4242: ⬆️ update to spring-boot-plugin-8.3.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ dependencies {
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.4.10")
 
   implementation("org.springframework.boot:spring-boot-starter-webflux")
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.9")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.10")
 
   implementation(platform("com.amazonaws:aws-java-sdk-bom:1.12.788"))
 }
