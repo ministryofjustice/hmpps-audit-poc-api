f27cc38f689a84e556682e77b0b3880e0a9952fb
Simon Mitchell <51327941+simon-mitchell@users.noreply.github.com>
2025-10-20 13:52:27 +0100

    HAAR-4426: ⬆️ update to spring-boot-plugin-9.1.3 (#149)
    * HAAR-4426: ⬆️ update to spring-boot-plugin-9.1.3
    
    * HAAR-4426: upgrade helm chart versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "9.1.2"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "9.1.3"
   kotlin("plugin.spring") version "2.2.20"
 }
 
@@ -10,12 +10,12 @@ configurations {
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-security")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.4.11")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.5.0")
 
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.13")
 
-  implementation(platform("com.amazonaws:aws-java-sdk-bom:1.12.791"))
+  implementation(platform("com.amazonaws:aws-java-sdk-bom:1.12.792"))
 }
 
 kotlin {
